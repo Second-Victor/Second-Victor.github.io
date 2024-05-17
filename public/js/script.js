721b4b6667a77740b074180a72c65ccd954cbb86
Allan Davis <allan.victor.davis@icloud.com>
2024-05-17 17:12:23 +0100

    fixed the font error

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+	distance: '90px',
+	duration: 2000,
+	delay: 450,
+	reset: false
+});
+
+sr.reveal('.logo',{delay: 200, origin: 'top'});
+sr.reveal('.sidebar',{delay: 400, origin: 'left'});
